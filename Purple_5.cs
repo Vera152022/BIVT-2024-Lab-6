098107363ea45911b108b1732a9a988e3716d4ef
Vera152022 <118746256+Vera152022@users.noreply.github.com>
2025-03-07 23:29:24 +0300

    Update Purple_5.cs

--- Purple_5.cs
+++ Purple_5.cs
@@ -25,15 +25,16 @@ namespace Lab_6
                 _animal = animal;
                 _characterTrait = characterTrait;
                 _oobject = oobject;
-                _answer = new string[] {_animal, _characterTrait, _oobject};
-                
+                _answer = new string[] { _animal, _characterTrait, _oobject };
+
             }
 
             public int CountVotes(Response[] responses, int questionNumber)
             {
                 if (responses == null || questionNumber < 1 || questionNumber > 3) return 0;
+                questionNumber--;
                 int total = 0;
-                foreach(var i in responses)
+                foreach (var i in responses)
                 {
                     if (i._answer[questionNumber] != "")
                         total++;
@@ -44,7 +45,7 @@ namespace Lab_6
             public void Print()
             {
                 Console.Write(Animal + " " + CharacterTrait + " " + Concept);
-                
+
             }
         }
         public struct Research
@@ -54,39 +55,39 @@ namespace Lab_6
 
             public string Name => _title;
             public Response[] Responses => _responses;
-        
+
             public Research(string title)
             {
                 _title = title;
                 _responses = new Response[0];
             }
-        public void Add(string[] answers)
-        {
-            if (answers == null || _responses == null) return;
-            string[] ans = new string[] { null, null, null };
-            for(int i = 0; i < Math.Min(3, answers.Length); i++)
+            public void Add(string[] answers)
             {
-                ans[i] = answers[i];
+                if (answers == null || _responses == null) return;
+                string[] ans = new string[] { null, null, null };
+                for (int i = 0; i < Math.Min(3, answers.Length); i++)
+                {
+                    ans[i] = answers[i];
+                }
+
+                var res = new Response[_responses.Length + 1];
+                Array.Copy(_responses, res, res.Length - 1);
+                res[res.Length - 1] = new Response(ans[0], ans[1], ans[2]);
+                _responses = res;
             }
 
-            var res = new Response[_responses.Length + 1];
-            Array.Copy(_responses, res, res.Length - 1);
-            res[res.Length - 1] = new Response(ans[0], ans[1], ans[2]);
-            _responses = res;
-        }
-
-        public string[] GetTopResponses(int question)
-        {
+            public string[] GetTopResponses(int question)
+            {
                 if (_responses == null || question < 1 || question > 3) return null;
-
+                
                 int total = 0;
                 int n = _responses.Length;
                 string[] answer = new string[n];
-                for(int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++)
                 {
                     int c = 1;
                     var array_1 = new string[] { _responses[i].Animal, _responses[i].CharacterTrait, _responses[i].Concept };
-                    for(int j = 0; j < i; j++)
+                    for (int j = 0; j < i; j++)
                     {
                         var array_2 = new string[] { _responses[j].Animal, _responses[j].CharacterTrait, _responses[j].Concept };
                         if (array_1[question - 1] == array_2[question - 1])
@@ -95,26 +96,26 @@ namespace Lab_6
                             break;
                         }
                     }
-                    if(c == 1 && array_1[question - 1] != null) 
+                    if (c == 1 && array_1[question - 1] != null)
                     {
                         int q = 0;
-                        for(int j = 0; j < total; j++)
+                        for (int j = 0; j < total; j++)
                         {
                             if (answer[j] == array_1[question - 1]) q++;
                         }
-                        if(q == 0 && array_1[question - 1] != null && array_1[question - 1] != "-")
+                        if (q == 0 && array_1[question - 1] != null && array_1[question - 1] != "-")
                         {
                             answer[total] = array_1[question - 1];
                             total++;
                         }
                     }
-                
+
                 }
                 int[] counts = new int[answer.Length];
-                for(int i = 0; i < answer.Length; i++)
-                {   
+                for (int i = 0; i < answer.Length; i++)
+                {
                     var answer_1 = new string[] { _responses[i].Animal, _responses[i].CharacterTrait, _responses[i].Concept };
-                    for (int j = 0; j < answer.Length; j++) 
+                    for (int j = 0; j < answer.Length; j++)
                     {
                         if (answer[j] != null && answer_1[question - 1] == answer[j])
                         {
@@ -129,11 +130,10 @@ namespace Lab_6
                 return total_answer;
             }
 
-        public void Print()
-        {
-        }
-        }
+            public void Print()
+            {
+            }
 
-        
+        }
     }
 }
